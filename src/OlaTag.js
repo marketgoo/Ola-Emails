e6e8cbcbc536431f69c29a9d63e95b272a438e00
griselmatosm <griselmatosm@gmail.com>
2020-11-27 10:59:19 +0100

    Fix Ola Tag

--- src/OlaTag.js
+++ src/OlaTag.js
@@ -6,52 +6,6 @@ const tokens = require('./tokens');
 //     'ola-panel': ['ola-tag'],
 // });
 
-const styles = {
-    default: {
-        'border-radius': tokens('radius-small'),
-        'font-weight': tokens('bold'),
-        'background-color': tokens('gray-xlight'),
-        'color': tokens('black'),
-        'display': 'inline-flex',
-        'align-items': 'center',
-        'white-space': 'nowrap',
-        'padding': `0 ${tokens}('size-3')`,
-        'vertical-align': 'middle',
-        'cursor': 'default',
-        'max-width': '100%',
-        'letter-spacing': '0',
-        'height': tokens('size-6'),
-    },
-    success: {
-        'background-color': tokens('success'),
-        'color': tokens('white'),
-    },
-    error: {
-        'background-color': tokens('error'),
-        'color': tokens('white')
-    },
-    warning: {
-        'background-color': tokens('warning'),
-        'color': tokens('white')
-    },
-    small: {
-        'height': tokens('size-6'),
-        'font': tokens('font-caption'),
-        'font-weight': tokens('bold')
-    },
-    medium: {
-        'height': tokens('size-7'),
-        'font': tokens('font-callout'),
-        'font-weight': tokens('bold')
-    },
-    big: {
-        'height': tokens('size-8'),
-        'font': tokens('font-body'),
-        'font-weight': tokens('bold'),
-        'padding': `0 ${tokens}('size-4')`
-    }
-};
-
 class OlaTag extends BodyComponent {
     static endingTag = true;
 
@@ -64,37 +18,32 @@ class OlaTag extends BodyComponent {
         size: 'small',
     };
 
+
     render() {
+
+        const background_color = this.getAttribute('variant') ? tokens(this.getAttribute('variant')) : tokens('gray-xlight');
+        const color = this.getAttribute('variant') ? 'white' : tokens('black');
+
+        const height = this.getAttribute('size') === 'medium' ? tokens('size-7') : this.getAttribute('size') === 'big' ? tokens('size-7') : tokens('size-6')
+
+        const font = this.getAttribute('size') === 'medium' ? 'callout' : this.getAttribute('size') === 'big' ? 'body' : 'caption'
+
+
         return `
-        <table class="ola_tag">
+        <table class="ola_tag" style="margin: auto">
             <tbody>
                 <tr>
-                    <td style="background:pink; padding: 2px 14px;border-radius:8px">
-                        ${this.renderMJML(`<ola-text variant="caption" font-weight="bold" align="center">${this.getContent()}</ola-text>`)}
+                    <td
+                    style="background-color:${background_color}; border-radius:${tokens('radius-small')}; padding: 0 ${tokens('size-3')}; display: inline-flex; align-items: center;white-space: nowrap; vertical-align: middle; cursor: default; max-width: 100%; letter-spacing: 0; height: ${height};"
+                    >
+                        ${this.renderMJML(`<ola-text font-weight="bold" align="center" color=${color} variant=${font} >
+                        ${this.getContent()}
+                        </ola-text>`)}
                     </td>
                 </tr>
             </tbody>
         </table>
 		`;
-    }
-
-    _render() {
-        const attributes = {
-            ...styles.default,
-            ...styles[this.getAttribute('variant')],
-            ...styles[this.getAttribute('size')],
-        };
-
-        return this.renderMJML(`
-            <mj-text
-            ${this.htmlAttributes({
-            ...attributes
-        })}
-            >
-                ${this.getContent()}
-            </mj-text>
-
-		`);
     }
 }
 
