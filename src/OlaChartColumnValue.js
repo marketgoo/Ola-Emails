3e0dca848618d69626e734b6d2397f5a656d116c
griselmatosm <griselmatosm@gmail.com>
2020-12-03 19:03:56 +0100

    Fix width to OlaChartColumnValue to be responsive

--- src/OlaChartColumnValue.js
+++ src/OlaChartColumnValue.js
@@ -20,6 +20,7 @@ class OlaChartColumnValue extends BodyComponent {
       .ola_chartColumn-value {
         vertical-align: bottom;
         padding: 0 ${tokens('size-2')};
+        width: 120px;
       }
 
       .ola_chartColumn-graphic {
@@ -35,7 +36,7 @@ class OlaChartColumnValue extends BodyComponent {
               <div class="ola_chartColumn-label">
                 ${this.renderMJML(`<ola-text variant="callout" align="center" color="gray" font-weight:"bold" > ${this.getContent()} </ola-text>`)}
               </div>
-              <div class="ola_chartColumn-graphic" style="height:${parseInt(tokens('size-10')) * this.getAttribute('value')}px; background-color: ${tokens(this.getAttribute('color'))}; width:120px"></div>
+              <div class="ola_chartColumn-graphic" style="height:${parseInt(tokens('size-10')) * this.getAttribute('value')}px; background-color: ${tokens(this.getAttribute('color'))};"></div>
             </td>
         `);
     }
