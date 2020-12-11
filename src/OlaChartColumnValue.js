c270e3b3e8bbbe7d2c248f8f0fee924578b93b1a
griselmatosm <griselmatosm@gmail.com>
2020-12-11 14:02:45 +0100

    Fix typo

--- src/OlaChartColumnValue.js
+++ src/OlaChartColumnValue.js
@@ -36,7 +36,7 @@ class OlaChartColumnValue extends BodyComponent {
         return (`
             <td class="ola_chartColumn-value" >
               <div class="ola_chartColumn-label">
-                ${this.renderMJML(`<ola-text variant="callout" align="center" color="gray" font-weight:"bold" > ${this.getAttribute('label')} </ola-text>`)}
+                ${this.renderMJML(`<ola-text variant="callout" align="center" color="gray" font-weight="bold" > ${this.getAttribute('label')} </ola-text>`)}
               </div>
               <div class="ola_chartColumn-graphic" style="height:${parseInt(tokens('size-10')) * this.getAttribute('value')}px; background-color: ${tokens(this.getAttribute('color'))};"></div>
             </td>
