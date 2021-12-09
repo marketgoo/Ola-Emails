2a54481e9d1c7fb44ed62d129dae15b3cf06e4fb
Oscar Otero <oom@oscarotero.com>
2021-12-09 19:37:27 +0100

    extend mj-column and mj-section

--- src/OlaColumn.js
+++ src/OlaColumn.js
@@ -1,27 +1,11 @@
 const { registerDependencies } = require('mjml-validator');
-const { BodyComponent } = require('mjml-core');
+const MJColumn = require('mjml-column');
 const tokens = require('./tokens');
 
-class OlaColumn extends BodyComponent {
-
-    static endingTag = true;
-
-    static allowedAttributes = {
-        'padding': 'string',
-        'background-color': 'enum(brand,white,black,gray,gray-light,gray-xlight,gray-xxlight,error,warning,success,pro,premium)',
-        'border-radius': 'string'
-    };
-
-    static defaultAttributes = {
-        'padding': '0'
-    };
-
-    render() {
-        return this.renderMJML(`
-            <mj-column css-class="ola_column" padding=${this.getAttribute('padding')} inner-background-color=${tokens(this.getAttribute('background-color'))} inner-border-radius=${this.getAttribute('border-radius')}>
-                ${this.getContent()}
-            </mj-column>
-        `)
+class OlaColumn extends MJColumn {
+    getAttribute(name) {
+        const value = super.getAttribute(name);
+        return tokens(value) || value;
     }
 }
 
--- src/OlaSection.js
+++ src/OlaSection.js
@@ -1,27 +1,31 @@
 const { registerDependencies } = require('mjml-validator');
-const { BodyComponent } = require('mjml-core');
+const MJSection = require('mjml-section');
 const tokens = require('./tokens');
 
-class OlaSection extends BodyComponent {
-
-    static endingTag = true;
-
-    static allowedAttributes = {
-        'padding': 'string',
-        'background-color': 'enum(brand,white,black,gray,gray-light,gray-xlight,gray-xxlight,error,warning,success,pro,premium)',
-    };
-
-    static defaultAttributes = {
-        'padding': '0'
-    };
-
-    render() {
-        return this.renderMJML(`
-            <mj-section css-class="ola_section" padding=${this.getAttribute('padding')} background-color=${tokens(this.getAttribute('background-color'))}>
-                ${this.getContent()}
-            </mj-section>
-        `)
+class OlaSection extends MJSection {
+    getAttribute(name) {
+        const value = super.getAttribute(name);
+        return tokens(value) || value;
     }
+
+    // static endingTag = true;
+
+    // static allowedAttributes = {
+    //     'padding': 'string',
+    //     'background-color': 'enum(brand,white,black,gray,gray-light,gray-xlight,gray-xxlight,error,warning,success,pro,premium)',
+    // };
+
+    // static defaultAttributes = {
+    //     'padding': '0'
+    // };
+
+    // render() {
+    //     return this.renderMJML(`
+    //         <mj-section css-class="ola_section" padding=${this.getAttribute('padding')} background-color=${tokens(this.getAttribute('background-color'))}>
+    //             ${this.getContent()}
+    //         </mj-section>
+    //     `)
+    // }
 }
 
 module.exports = OlaSection;
