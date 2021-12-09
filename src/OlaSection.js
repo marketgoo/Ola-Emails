a2847bfaa3c58e4976bfcc240c62b2cf13d03cf8
griselmatosm <griselmatosm@gmail.com>
2021-12-09 20:02:40 +0100

    cleaning code

--- src/OlaSection.js
+++ src/OlaSection.js
@@ -7,25 +7,6 @@ class OlaSection extends MJSection {
         const value = super.getAttribute(name);
         return tokens(value) || value;
     }
-
-    // static endingTag = true;
-
-    // static allowedAttributes = {
-    //     'padding': 'string',
-    //     'background-color': 'enum(brand,white,black,gray,gray-light,gray-xlight,gray-xxlight,error,warning,success,pro,premium)',
-    // };
-
-    // static defaultAttributes = {
-    //     'padding': '0'
-    // };
-
-    // render() {
-    //     return this.renderMJML(`
-    //         <mj-section css-class="ola_section" padding=${this.getAttribute('padding')} background-color=${tokens(this.getAttribute('background-color'))}>
-    //             ${this.getContent()}
-    //         </mj-section>
-    //     `)
-    // }
 }
 
 module.exports = OlaSection;
