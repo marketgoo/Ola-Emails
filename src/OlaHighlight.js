947c362d15613825121af637dc37774dcbf1a112
griselmatosm <griselmatosm@gmail.com>
2021-12-09 11:08:59 +0100

    fix: OlaHighlight component

--- src/OlaHighlight.js
+++ src/OlaHighlight.js
@@ -9,32 +9,26 @@ class OlaHighlight extends BodyComponent {
     static allowedAttributes = {
         'background-color': 'enum(brand,white,black,gray,gray-light,gray-xlight,gray-xxlight,error,warning,success,pro,premium)',
         'padding': 'string',
-        'outer-padding': 'string',
         'border-radius': 'string'
     };
 
     static defaultAttributes = {
-        'background-color': 'gray-xxlight',
         'border-radius': '0',
-        'padding': '0',
-        'outer-padding': '0'
+        'padding': '0'
     };
 
     headStyle() {
         return `
-    .ola_highlight-section {
+    .ola_highlight {
         min-height: 215px;
     }
-
     `;
     }
 
     render() {
         return this.renderMJML(`
-            <mj-wrapper css-class="ola_highlight" padding=${this.getAttribute('outer-padding')}>
-                <mj-section css-class="ola_highlight-section" padding=${this.getAttribute('padding')} background-color=${tokens(this.getAttribute('background-color'))} border-radius=${this.getAttribute('border-radius')}>
-                    ${this.getContent()}
-                </mj-section>
+            <mj-wrapper css-class="ola_highlight" padding=${this.getAttribute('padding')} border-radius=${this.getAttribute('border-radius')} background-color=${tokens(this.getAttribute('background-color'))}>
+                ${this.getContent()}
             </mj-wrapper>
         `)
     }
