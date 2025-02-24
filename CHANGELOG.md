ba8d53f90c0322bda4a750491ae54a2c18c46bfb
Oscar Otero <oom@oscarotero.com>
2025-02-24 12:07:06 +0100

    improve olaissue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.0.6] - 2025-02-24
+### Added
+- `line-top` and `size` attributes to `OlaIssue`.
+
 ## [1.0.5] - 2025-02-24
 ### Added
 - Bottom line to the charts columns to be visible with zero value.
@@ -240,6 +244,7 @@ First version
 [#35]: https://github.com/marketgoo/Ola-Emails/issues/35
 [#36]: https://github.com/marketgoo/Ola-Emails/issues/36
 
+[1.0.6]: https://github.com/marketgoo/Ola-Emails/compare/v1.0.5...v1.0.6
 [1.0.5]: https://github.com/marketgoo/Ola-Emails/compare/v1.0.4...v1.0.5
 [1.0.4]: https://github.com/marketgoo/Ola-Emails/compare/v1.0.3...v1.0.4
 [1.0.3]: https://github.com/marketgoo/Ola-Emails/compare/v1.0.2...v1.0.3
--- src/OlaIssue.js
+++ src/OlaIssue.js
@@ -7,16 +7,20 @@ class OlaIssue extends BodyComponent {
 
     static allowedAttributes = {
         icon: 'string',
+        size: 'enum(small,big)',
+        "line-top": 'enum(true,false)',
     };
+    static defaultAttributes = {
+      size: 'big',
+      "line-top": false,
+    }
 
     headStyle(breakpoint) {
         return `
       .ola_task {
         padding-right: ${tokens('size-5')};
         padding-left: ${tokens('size-5')};
-      }
-      .ola_task td {
-          padding: ${tokens('size-2')} 0;
+        width: 100%;
       }
       @media only screen and (max-width:${breakpoint}) {
         .ola_task {
@@ -28,20 +32,25 @@ class OlaIssue extends BodyComponent {
     }
 
     render() {
+      const size = this.getAttribute('size') || 'big';
+      const iconSize = size === 'small' ? 24 : 28;
+      const fontSize = size === 'small' ? 'font-1-regular' : 'font-2-regular';
+      const padding = size === 'small' ? 'size-1' : 'size-2';
+      const lineTop = this.getAttribute('line-top') ? `border-top: 1px solid ${tokens("color-neutral-200")};` : '';
+
         return `
-        <table class="ola_task" style="vertical-align:middle;text-align=left">
+        <table class="ola_task" style="vertical-align:middle;text-align:left;${lineTop}">
             <tbody>
                     <tr>
-                        <td style="width:44px;vertical-align:top;">
+                        <td style="width:${iconSize + 10}px;vertical-align:top;padding:${tokens(padding)} 0;">
                                 <img
                                     src="${this.getAttribute('icon')}"
-                                    width="28"
-                                    style="font-8-medium:block;"
+                                    width="${iconSize}"
                                     >
                         </td>
-                        <td style="vertical-align:middle;">
+                        <td style="vertical-align:middle;padding:${tokens(padding)} 0;">
                             ${this.renderMJML(`
-                            <ola-text variant="font-2-regular">
+                            <ola-text variant="${fontSize}">
                                 ${this.getContent()}
                             </ola-text>
                             `)}
