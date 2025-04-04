b85aff41a4f446ec8afe7241eaadac98ba4664b4
Xenock <miguelmoracho@gmail.com>
2025-04-04 11:15:00 +0200

    feat: left aligned header components

--- src/OlaCardHeader.js
+++ src/OlaCardHeader.js
@@ -3,6 +3,7 @@ const { BodyComponent } = require("mjml-core");
 const tokens = require("./tokens");
 
 registerDependencies({
+  "ola-card": ["ola-card-header"],
   "mj-wrapper": ["ola-card-header"],
 });
 
@@ -21,17 +22,47 @@ class OlaCardHeader extends BodyComponent {
     color: "color-neutral-700",
   };
 
+  headStyle() {
+    return `
+      .ola_card-header {
+        display: table;
+        width: 100%;
+      }
+      
+      .ola_card-header-icon {
+        display: table-cell;
+        vertical-align: middle;
+        padding-right: 4px;
+      }
+      
+      .ola_card-header-text {
+        display: table-cell;
+        vertical-align: middle;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 100%;
+      }
+    `;
+  }
+
   render() {
     return this.renderMJML(`
-            <mj-section padding="16px">
-                <mj-column width="20%" vertical-align="middle">
-                    <mj-image width="${this.getAttribute("icon-width")}" padding="0" src="${this.getAttribute("src")}" />
-                </mj-column>
-                <mj-column width="80%" vertical-align="middle">
-                    <ola-text variant="font-1-regular" color="${this.getAttribute("color")}">${this.getAttribute("text")}</ola-text>
-                </mj-column>
-            </mj-section>
-        `);
+      <mj-section padding="16px">
+        <mj-column>
+          <mj-table css-class="ola_card-header" padding="0" width="100%">
+            <tr>
+              <td class="ola_card-header-icon" style="width: ${this.getAttribute("icon-width")};">
+                <img width="${this.getAttribute("icon-width")}" src="${this.getAttribute("src")}" style="display: block;" />
+              </td>
+              <td class="ola_card-header-text">
+                <ola-text variant="font-1-regular" color="${this.getAttribute("color")}">${this.getAttribute("text")}</ola-text>
+              </td>
+            </tr>
+          </mj-table>
+        </mj-column>
+      </mj-section>
+    `);
   }
 }
 
