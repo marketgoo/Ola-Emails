c735c385b34a28bcebbe39e8d1f7bcb029b9fe55
Xenock <miguelmoracho@gmail.com>
2025-04-22 11:21:02 +0200

    feat: added 2 more sizes to divider

--- src/OlaDivider.js
+++ src/OlaDivider.js
@@ -11,7 +11,7 @@ class OlaDivider extends BodyComponent {
 
     static allowedAttributes = {
         color: 'enum(color-primary-500,color-white,color-neutral-900,color-neutral-700,color-neutral-400,color-neutral-200,color-neutral-100)',
-        margin: 'enum(none,small,normal)',
+        margin: 'enum(none,extra-small,smaller,small,normal)',
     };
 
     static defaultAttributes = {
@@ -21,12 +21,23 @@ class OlaDivider extends BodyComponent {
 
     render() {
         const color = tokens(this.getAttribute('color'));
-        const margin =
-            this.getAttribute('margin') === 'small'
-                ? tokens('size-3')
-                : this.getAttribute('margin') === 'normal'
-                  ? tokens('size-4')
-                  : '0';
+        const marginType = this.getAttribute('margin');
+        let margin = '0';
+
+        switch (marginType) {
+            case 'extra-small':
+                margin = tokens('size-1');
+                break;
+            case 'smaller':
+                margin = tokens('size-2');
+                break;
+            case 'small':
+                margin = tokens('size-3');
+                break;
+            case 'normal':
+                margin = tokens('size-4');
+                break;
+        }
 
         return `
         <p style="border-top:solid 4px ${color};font-size:1px;margin:${margin} auto;width:100%;">
